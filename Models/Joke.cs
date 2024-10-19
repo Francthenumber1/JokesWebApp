510603b9e3b3e5cea6fd18e02777938e07b4d7b6
franc <aubinnjoutsa1@gmail.com>
2024-10-19 14:01:11 +0200

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string JokeQuestion{ get; set; }
+
+        public string JokeAnswer { get; set; }
+        public Joke()
+        {
+            
+        }
+
+
+
+        
+            
+        
+    }
+}
